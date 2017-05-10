7e4ce7d1d02d31f1da4ac8f9994ea751b9d0ca7c
Runner <841766635@qq.com>
2017-05-10 23:48:28 +0800

    自己写一个对象观察

---
+++ question/1how_to_observe_object.js
@@ -0,0 +1,46 @@
+function Observe(data) {
+	this.data = data;
+	this.walk(data);
+}
+
+Observe.prototype.walk = function(data) {
+	var key, val;
+	for (key in data) {
+		if (!data.hasOwnProperty(key)) {
+			continue;
+		}
+		val = data.key;
+		if (Object.prototype.toString.call(val) == '[object Object]') {
+			new Observe(val);
+		} else {
+			this.defineData(key, val);
+		}
+	}
+}
+Observe.prototype.defineData = function(key, val) {
+	Object.defineProperty(this.data, key, {
+		enumerable: true,
+		configurable: true,
+		get: function() {
+			console.log('you visited ' + key + ',the value is ' + val);
+			return val;
+		},
+		set: function(newVal) {
+			console.log('you set ' + key + ' a new value "' + newVal + '" intead of "' + val + '"');
+
+			val = newVal;
+		}
+	})
+
+
+};
+
+
+
+var data = {
+	a: 1,
+	b: 2
+};
+var ob_data = new Observe(data);
+ob_data.a;
+ob_data.b = 5;
